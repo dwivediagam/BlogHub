16e2fed070d1a665b4f2ee625643459e3eee5819
agam98 <dwivedi.agam233@gmail.com>
2017-05-21 10:22:40 +0000

    Added the edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully edited"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :description)
