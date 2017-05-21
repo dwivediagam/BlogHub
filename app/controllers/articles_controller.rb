78eacf45e773f8fbe04ee5a2476adcc9e8a0a357
agam98 <dwivedi.agam233@gmail.com>
2017-05-21 10:05:41 +0000

    Added new and show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,16 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to Article.show(@article)
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
