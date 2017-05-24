08f42501af6fc6fd5a96cd827fb718831a11afd7
agam98 <dwivedi.agam233@gmail.com>
2017-05-24 09:41:52 +0000

    Make sessions controller for login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in!"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Something went wrong with your login information"
+      render 'new'
+    end
   end
   
   def destroy
-  
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out!"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
