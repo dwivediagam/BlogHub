22019b90aacb37b09af0051a4cb16d00cdb68c86
agam98 <dwivedi.agam233@gmail.com>
2017-05-24 16:31:50 +0000

    Establish link between login and logout page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :update]
   def new
     @user = User.new
   end
@@ -18,16 +19,13 @@ class UsersController < ApplicationController
   end
   
   def edit 
-    @user = User.find(params[:id])
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully!"
       redirect_to articles_path
@@ -41,5 +39,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
