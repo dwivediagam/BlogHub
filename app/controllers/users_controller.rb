7562b6e78619cfccdda3257077fd6834dd3f22e3
agam98 <dwivedi.agam233@gmail.com>
2017-05-25 12:44:06 +0000

    Completed admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :show, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   def new
     @user = User.new
   end
@@ -27,6 +28,12 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
   def update
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully!"
@@ -45,8 +52,14 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
     def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger] = "You can only delete and edit your own account"
+        redirect_to root_path
+      end
+    end
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "Only admin users can perform this action"
         redirect_to root_path
       end
     end
