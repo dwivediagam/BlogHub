2c6a88992b50a9aff97ab22a08671e4e71f687f1
agam98 <dwivedi.agam233@gmail.com>
2017-05-26 05:25:19 +0000

    Integration Test for INvalid Category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -11,4 +11,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
   
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, category: {name: " "}
+    end
+    assert_template 'categories/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  
 end
